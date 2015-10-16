a82ca860410b01d9a814002a9c71aa5492bd8314
Michael L Heuer <heuermh@acm.org>
2015-10-16 15:18:44 -0500

    fix scala 2.11 scripts

--- scripts/move_to_scala_2.10.sh
+++ scripts/move_to_scala_2.10.sh
@@ -3,8 +3,8 @@
 set +x
 
 find . -name "pom.xml" -exec sed \
-     -e "s/\<artifactId\>adam-plugins_2.11\<\/artifactId\>/\<artifactId\>adam-plugins_2.10\<\/artifactId\>/g" \
-     -e "s/\<name\>adam-plugins_2.11\<\/name\>/\<name\>adam-plugins_2.10\<\/name\>/g" \
+     -e "s/\<artifactId\>adam-commands_2.11\<\/artifactId\>/\<artifactId\>adam-commands_2.10\<\/artifactId\>/g" \
+     -e "s/\<name\>adam-commands_2.11\<\/name\>/\<name\>adam-commands_2.10\<\/name\>/g" \
      -e "s/\<scala.version\>2.11.4\<\/scala.version\>/\<scala.version\>2.10.4\<\/scala.version\>/g" \
      -e "s/\<scala.artifact.suffix\>2.11\<\/scala.artifact.suffix\>/\<scala.artifact.suffix\>2.10\<\/scala.artifact.suffix\>/g" \
      -i .2.10.bak '{}' \;
